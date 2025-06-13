b610940ffc0ce9a87ad492d19501895e8ac0e6e2
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-06-13 11:22:34 -0300

    update

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello")
+import requests
+import json
+
+from headers import *
+url = "https://real-time-amazon-data.p.rapidapi.com/deals-v2"
+
+querystring = {"country":"US","categories":"eletronic","min_product_star_rating":"ALL","price_range":"ALL","discount_range":"ALL"}
+
+headers = {
+	"x-rapidapi-key": key,
+	"x-rapidapi-host": host
+}
+
+response = requests.get(url,headers=headers,params=querystring)
+response_json = response.json()
+
+with open("amazon-data.json","w") as f:
+	json.dump(response_json,f,indent=4)
